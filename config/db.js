958d629d106d556532d4f19c95a0af39402eedce
Chris666 <g.a_cristo42@hotmail.com>
2024-08-03 15:34:35 -0600

    fix

--- config/db.js
+++ config/db.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 mongoose.connection.on('error', (error) => {
-    console.log(error);
+    console.error('Error de conexión:', error);
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Conectado a MongoDB');
 });
 
 // Importar los modelos
