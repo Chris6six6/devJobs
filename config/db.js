2267ea5d15561d0bf4dc68914bc7de98a891c886
Chris666 <g.a_cristo42@hotmail.com>
2024-08-03 15:50:23 -0600

    fix

--- config/db.js
+++ config/db.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
-mongoose.connect(process.env.DATABASE, {
-  serverSelectionTimeoutMS: 30000, // Aumenta el tiempo de espera a 30 segundos
-  ssl: true // Asegúrate de que SSL esté habilitado
-});
+mongoose.connect(process.env.DATABASE);
 
 mongoose.connection.on('error', (error) => {
-    console.error('Error de conexión:', error);
-});
-
-mongoose.connection.once('open', () => {
-    console.log('Conectado a MongoDB');
+    console.log(error);
 });
 
 // Importar los modelos
