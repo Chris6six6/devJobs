29be89b6b01876d2341e1edfb74226435fa2a00b
Chris666 <g.a_cristo42@hotmail.com>
2024-08-03 16:01:46 -0600

    fix

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const mongoose = require('mongoose');
-require('./config/db.js');
-
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const router = require('./routes');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -12,20 +8,15 @@ const bodyParser = require('body-parser');
 const flash = require('connect-flash');
 const createError = require('http-errors');
 const passport = require('./config/passport');
-
+const router = require('./routes');
 require('dotenv').config({ path: 'variables.env' });
 
 const app = express();
 
-// Habilitar body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Configurar el directorio de vistas
+app.set('views', path.join(__dirname, 'views'));
 
-// Validacion de campos
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Configurar Handlebars para permitir el acceso a propiedades del prototipo
+// Configurar Handlebars
 const hbs = exphbs.create({
     defaultLayout: 'layout',
     helpers: require('./helpers/handlebars'),
@@ -35,12 +26,15 @@ const hbs = exphbs.create({
     }
 });
 
-// Habilitar Handlebars como view engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Configurar body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
@@ -61,7 +55,7 @@ app.use(passport.session());
 // Alertas y flash messages
 app.use(flash());
 
-// Crear nuestro middleware
+// Middleware personalizado
 app.use((req, res, next) => {
     res.locals.mensajes = req.flash();
     next();
@@ -69,21 +63,19 @@ app.use((req, res, next) => {
 
 app.use('/', router());
 
-// 404 Pagina no encontrada
+// 404 Página no encontrada
 app.use((req, res, next) => {
-    next(createError(404, 'No encontado'));
-})
+    next(createError(404, 'No encontrado'));
+});
 
-// Administracion de los errores
+// Administrar los errores
 app.use((error, req, res, next) => {
     res.locals.mensaje = error.message;
-
     res.render('error');
-})
+});
 
 // Definir un puerto y arrancar el proyecto
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
-    {
-        console.log(`El servidor esta funcionando en el puerto: ${port}`)
-    });
+app.listen(port, () => {
+    console.log(`El servidor está funcionando en el puerto: ${port}`);
+});
